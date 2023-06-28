aecefe5e13695f6e06f27eb7ea6ed84880c4834b
LuciFerrr07 <115144507+LuciFerrr07@users.noreply.github.com>
2023-06-28 12:26:15 +0530

    Update revoke_security_group_modification.py

--- revoke_security_group_modification.py
+++ revoke_security_group_modification.py
@@ -3,10 +3,11 @@ import json
 
 def lambda_handler(event, context):
     ec2=boto3.client('ec2')
-    #Retrieve the modified security group details from the event
-    modified_security_group = event['detail']['requestParameters']['groupId']
-    ip_permissions = event['detail']['requestParameters']['ipPermissions']['items']
-    sg_rule=event['detail']['responseElements']['securityGroupRuleSet']['items']
+    event=json.loads(event['body'])
+    res = event['result'].get('_raw');
+    res = json.loads(res)
+    modified_security_group = res['requestParameters']['groupId']
+    sg_rule=res['responseElements']['securityGroupRuleSet']['items']
     for i in sg_rule:
         if i['cidrIpv4']=='0.0.0.0/0':
             response = ec2.revoke_security_group_ingress(GroupId=modified_security_group, SecurityGroupRuleIds=[i['securityGroupRuleId']])
